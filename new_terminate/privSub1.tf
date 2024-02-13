b24731dc81a26de9579fc4d60146348ae983bdf0
sergeivavilov <vss1@bk.ru>
2024-02-13 16:20:35 -0700

    added terraform task wordpress

--- new_terminate/privSub1.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.wordpress_vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "private_subnet_1"
-  }
-}
--- new_terminate/privSub2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.wordpress_vpc.id
-  cidr_block        = "10.0.5.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "private_subnet_2"
-  }
-}
